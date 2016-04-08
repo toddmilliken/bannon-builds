7ccd99c218f86890485352c87d5031eba40b3ab6
Todd M <milliktr@gmail.com>
2016-04-07 22:30:02 -0400

    Add responsive imagery crops to panels. Remove loading icon.

--- wp-content/themes/custom-theme/partials/front-page/panels.php
+++ wp-content/themes/custom-theme/partials/front-page/panels.php
@@ -1,7 +1,7 @@
 <?php if ( $panels = get_field('home_panels') ) : ?>
 	
 	<section class="panels-wrapper">
-		<div class="icon-loading"><i class="fa fa-circle-o-notch fa-spin"></i></div>
+		<!--<div class="icon-loading"><i class="fa fa-circle-o-notch fa-spin"></i></div>-->
 		<div class="arrow--down"><i class="fa fa-angle-down"></i></div>
 		<div class="panels owl-carousel">
 	
@@ -13,13 +13,11 @@
 			<div class="panel panel-<?php echo $panel_counter; ?>" id="panel-<?php echo $panel_counter; ?>">
 				
 				<?php if ( $bg_img = $panel['hp_bg_img'] ) :	?>
-<!--
 					<div class="panel-bg picturefill-background">
-						<span data-src="<?php echo $bg_img['url']; ?>" data-media="(min-width: 681px)"></span>
+						<span data-src="<?php echo $bg_img['sizes']['desktop']; ?>" data-media="(min-width: 1025px)"></span>
+						<span data-src="<?php echo $bg_img['sizes']['tablet']; ?>" data-media="(max-width: 1024px)"></span>
 						<span data-src="<?php echo $bg_img['sizes']['mobile']; ?>" data-media="(max-width: 680px)"></span>
 					</div>
--->
-					<img class="panel__img" src="<?php echo $bg_img['url']; ?>"/>
 				<?php endif; ?>
 
 				<div class="hp-content">
